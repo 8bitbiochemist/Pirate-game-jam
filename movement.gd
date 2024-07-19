113f45e6f1bc9a97156a54888752a3b29cfbfd5c
8bitbiochemist <8bitbiochemist@gmail.com>
2024-07-19 16:35:08 +0100

    Player movement and attacks have been added and the character sprite has been changed

--- movement.gd
+++ movement.gd
@@ -4,11 +4,10 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		position.x += speed * delta
 	if Input.is_action_pressed("move_left"):
@@ -17,5 +16,21 @@ func _process(delta):
 		position.y += speed * delta
 	if Input.is_action_pressed("move_up"):
 		position.y -= speed * delta
+	attacks()
+
+
+func attacks():
+	if Input.is_action_pressed("weapon_attack"):
+		$"Testing Attacks".text = "Melee"
+	elif Input.is_action_pressed("spell_1"):
+		$"Testing Attacks".text = "Spell 1"
+	elif Input.is_action_pressed("spell_2"):
+		$"Testing Attacks".text = "Spell 2"
+	else:
+		$"Testing Attacks".text = "No Input"
+	
+	
+	
 		
 	
+		
