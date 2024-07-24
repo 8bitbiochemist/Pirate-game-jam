f15764bd0d4db692c2e8f440ae78b6b802c8a511
8bitbiochemist <8bitbiochemist@gmail.com>
2024-07-24 19:08:17 +0100

    navigation agent difficult to work out

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,18 +3,42 @@ extends CharacterBody2D
 @export var speed = 150 #speed of enemy
 var player_chase = false #boolean that tells enemy to chase the player or not
 var player = null
-@onready var nav: NavigationAgent2D = $NavigationAgent2D #navigationagent2d import
+@onready var navi_agent: NavigationAgent2D = $NavigationAgent2D #navigationagent2d import
+var movement_target_position : Vector2 = Vector2(767,336)
 
-func _physics_process(_delta):
-	if player_chase: 
-		position += (player.position - position) / speed
+func _ready():
+	navi_agent.path_desired_distance = 4.0
+	navi_agent.target_desired_distance = 4.0
+	
+	call_deferred("actor_setup")
+	
+func actor_setup():
+		await get_tree().physics_frame
+		set_movement_target(movement_target_position)
 		
-func _on_detection_body_entered(body): # function that makes the enemy chase the player when player 
+		
+
+func set_movement_target(movement_target: Vector2):
+	navi_agent.target_position = movement_target
+	
+	
+func _physics_process(_delta):
+	if navi_agent.is_navigation_finished():
+		return
+	velocity = Vector2.ZERO
+	if player_chase:
+		var current_agent_position: Vector2 = global_position
+		var next_path_position: Vector2 = navi_agent.get_next_path_position()
+		velocity = position.direction_to(player.position) * speed
+		velocity = current_agent_position.direction_to(next_path_position) * speed
+		move_and_slide()
+		
+func _on_detection_body_entered(_body): # function that makes the enemy chase the player when player 
 									   # is in th collision shape
-	player = body
+	player = _body
 	player_chase = true
 
-func _on_detection_body_exited(body):  # function that makes the enemy chase the player when player 
+func _on_detection_body_exited(_body):  # function that makes the enemy chase the player when player 
 									   # is in th collision shape
 	player = null
 	player_chase = false
