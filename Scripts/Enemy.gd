e35d46d379aca0d3fa34b95ec61c153e538736de
8bitbiochemist <8bitbiochemist@gmail.com>
2024-07-28 21:32:21 +0100

    Health Component made

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,34 +3,12 @@ extends CharacterBody2D
 @export var speed = 150 #speed of enemy
 var player_chase = false #boolean that tells enemy to chase the player or not
 var player = null
-@onready var navi_agent: NavigationAgent2D = $NavigationAgent2D #navigationagent2d import
 var movement_target_position : Vector2 = Vector2(767,336)
-
-func _ready():
-	navi_agent.path_desired_distance = 4.0
-	navi_agent.target_desired_distance = 4.0
-	
-	call_deferred("actor_setup")
-	
-func actor_setup():
-		await get_tree().physics_frame
-		set_movement_target(movement_target_position)
-		
-		
-
-func set_movement_target(movement_target: Vector2):
-	navi_agent.target_position = movement_target
-	
 	
 func _physics_process(_delta):
-	if navi_agent.is_navigation_finished():
-		return
 	velocity = Vector2.ZERO
 	if player_chase:
-		var current_agent_position: Vector2 = global_position
-		var next_path_position: Vector2 = navi_agent.get_next_path_position()
 		velocity = position.direction_to(player.position) * speed
-		velocity = current_agent_position.direction_to(next_path_position) * speed
 		move_and_slide()
 		
 func _on_detection_body_entered(_body): # function that makes the enemy chase the player when player 
---
+++ Scripts/health_component.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+const MAX_HEALTH = 5
+var current_health = MAX_HEALTH
+
+
+func _ready():
+	update_health_ui()
+	$health_bar.max_value = MAX_HEALTH
+	
+func update_health_ui():
+	set_health_bar()
+
+func set_health_bar():
+	$health_bar.value = current_health
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("damage_test"):
+		damage()
+
+func damage():
+	current_health -= 1
+	if current_health <= 0:
+		current_health = MAX_HEALTH
+	update_health_ui()
